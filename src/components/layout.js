541722919c2a11f1f75a91b5e2d1a70ddef1eeda
Motoki Wu <tokestermw@gmail.com>
2019-03-16 17:29:01 -0700

    Added editor to website and made it grid layout.

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import CardHeader from '@material-ui/core/CardHeader';
+import Grid from '@material-ui/core/Grid'
 
 import Header from "./header"
 import "./layout.css"
@@ -24,24 +26,36 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
+      <Grid
+        container
+        spacing={4}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{
+          minHeight: '100vh',
+          minWidth: 100,
+        }}
+      >
+        <CardHeader title={data.site.siteMetadata.title} />
+        {/* <div
           style={{
             margin: `0 auto`,
             maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
             paddingTop: 0,
           }}
-        >
-          <main>{children}</main>
-          <footer>
+        > */}
+          <Grid item xs={6}>
+            <main>{children}</main>
+          </Grid>
+          {/* <footer>
             © {new Date().getFullYear()}, Built with
             {` `}
             <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
+          </footer> */}
+        {/* </div> */}
+      </Grid>
     )}
   />
 )
